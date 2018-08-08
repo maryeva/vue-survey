4d482396bfffb13812b8e7938bfdf8387dd4307b
Andrew Shield <andrewcshield@gmail.com>
2018-08-08 18:53:18 +0100

    Use compiled style sheet from ui as a static asset

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import '@thrivadev/thriva-ui/dist/ui.css'
+import './assets/styles/thriva-ui.css'
 import Vue from 'vue'
 import App from './App'
 import { sync } from 'vuex-router-sync'
