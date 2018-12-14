2c1b714b2bf35775bb5d0fbb988015390b5db499
Andrew Shield <andrewcshield@gmail.com>
2018-12-14 16:09:27 +1300

    Remove unnecessary files

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,12 +1,4 @@
 import Vue from 'vue'
-
-import VeeValidate from 'vee-validate'
-import validationConfig from '@/config/validation.js'
-
-import '@/filters/filters'
-import './factories/factories'
 import './mocks/matchMediaMock'
-
-Vue.use(VeeValidate, validationConfig)
 
 Vue.config.productionTip = false
